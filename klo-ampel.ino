446ff531d2ff2002eb3205783a7e618ba9b9a6b2
Manfred Mjka <manixx90@gmail.com>
2021-01-31 20:25:25 +0100

    chore: add debug information and config update

--- klo-ampel.ino
+++ klo-ampel.ino
@@ -8,19 +8,66 @@ const int redPin = 3;
 const int yellowPin = 4; 
 const int greenPin = 5; 
 const int statusPin = 6; 
-const int cycleInterval = 500;    // in ms
-const int hysteresis = 4;         // in cycles
-const int changeLightDelay = 850; // in ms
-const int turnOffDelay = 2000;    // in ms
+const int cycleInterval = 800;    // in ms
+const int hysteresis = 6;         // in cycles
+const int changeLightDelay = 900; // in ms
+const int turnOffDelay = 2500;    // in ms
 const int interruptedState = LOW; // what state describes interrupted
+const int ledHIGH = HIGH; 
+const int ledLOW = LOW; 
+const bool enableStatus = false; 
 
 bool someoneEntered = false; 
 bool isOccupied = false; 
 int cycleCount = 0; 
 
+void printStatus() {
+	Serial.print("someoneEntered: "); 
+	Serial.print(someoneEntered); 
+
+	Serial.print("\tisOccupied: "); 
+	Serial.print(isOccupied); 
+
+	Serial.print("\tcycleCount: "); 
+	Serial.println(cycleCount); 
+}
+
+void printConfig() {
+	Serial.println("==> Configuration"); 
+	Serial.print("gatePin: "); 
+	Serial.println(gatePin); 
+	Serial.print("redPin: "); 
+	Serial.println(redPin); 
+	Serial.print("yellowPin: "); 
+	Serial.println(yellowPin); 
+	Serial.print("redPin: "); 
+	Serial.println(redPin); 
+	Serial.print("statusPin: "); 
+	Serial.print(statusPin); 
+	Serial.print(", enabled: "); 
+	Serial.println(enableStatus); 
+
+	Serial.print("cycleInterval (ms): "); 
+	Serial.println(cycleInterval); 
+	Serial.print("hysteresis (cycles): "); 
+	Serial.println(hysteresis); 
+	Serial.print("changeLightDelay (ms): "); 
+	Serial.println(changeLightDelay); 
+	Serial.print("interruptedState: "); 
+	Serial.println(interruptedState); 
+	Serial.print("ledHIGH: "); 
+	Serial.print(ledHIGH); 
+	Serial.print(", ledLOW: "); 
+	Serial.println(ledLOW); 
+
+	Serial.println("\n === Start obervation now === \n"); 
+}
+
 void setup() {
 	Serial.begin(9600); 
 
+	printConfig(); 
+	
 	pinMode(redPin, OUTPUT); 
 	pinMode(yellowPin, OUTPUT); 
 	pinMode(greenPin, OUTPUT); 
@@ -30,9 +77,9 @@ void setup() {
 }
 
 void reset() {
-	digitalWrite(redPin, LOW); 
-	digitalWrite(yellowPin, LOW); 
-	digitalWrite(greenPin, LOW); 
+	digitalWrite(redPin, ledLOW); 
+	digitalWrite(yellowPin, ledLOW); 
+	digitalWrite(greenPin, ledLOW); 
 }
 
 void readGateSensor() {
@@ -57,35 +104,38 @@ void readGateSensor() {
 void runEnterSequence() {
 	reset(); 
 
-	digitalWrite(greenPin, HIGH); 
+	digitalWrite(greenPin, ledHIGH); 
 	delay(changeLightDelay); 
-	digitalWrite(greenPin, LOW); 
+	digitalWrite(greenPin, ledLOW); 
 	delay(changeLightDelay); 
-	digitalWrite(greenPin, HIGH); 
+	digitalWrite(greenPin, ledHIGH); 
 	delay(changeLightDelay); 
-	digitalWrite(greenPin, LOW); 
-	digitalWrite(yellowPin, HIGH); 
+	digitalWrite(greenPin, ledLOW); 
+	digitalWrite(yellowPin, ledHIGH); 
 	delay(changeLightDelay); 
-	digitalWrite(yellowPin, LOW); 
-	digitalWrite(redPin, HIGH); 
+	digitalWrite(yellowPin, ledLOW); 
+	digitalWrite(redPin, ledHIGH); 
 }
 
 void runLeaveSequence() {
 	reset(); 
 
-	digitalWrite(redPin, HIGH); 
+	digitalWrite(redPin, ledHIGH); 
 	delay(changeLightDelay); 
-	digitalWrite(yellowPin, HIGH); 
+	digitalWrite(yellowPin, ledHIGH); 
 	delay(changeLightDelay); 
-	digitalWrite(redPin, LOW); 
-	digitalWrite(yellowPin, LOW); 
-	digitalWrite(greenPin, HIGH); 
+	digitalWrite(redPin, ledLOW); 
+	digitalWrite(yellowPin, ledLOW); 
+	digitalWrite(greenPin, ledHIGH); 
 
 	delay(turnOffDelay); 
 	reset(); 
 }
 
 void triggerStatus() {
+	if(!enableStatus) 
+		return; 
+
 	digitalWrite(statusPin, HIGH); 
 	delay(100); 
 	digitalWrite(statusPin, LOW); 
@@ -93,14 +143,17 @@ void triggerStatus() {
 
 void loop() {
 	readGateSensor(); 
+	printStatus(); 
 
 	if(someoneEntered && !isOccupied) {
 		isOccupied = true; 
+		Serial.println("\n==> Running enter sequence\n"); 
 		runEnterSequence(); 
 	}
 
 	if(!someoneEntered && isOccupied) {
 		isOccupied = false; 
+		Serial.println("\n==> Running leave sequence\n"); 
 		runLeaveSequence(); 
 	}
 	
